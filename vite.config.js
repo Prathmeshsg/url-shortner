b644107147cfc765efad9e603269e9eb89af5e2b
Prathmesh Gandhale <125843181+Shadowgit98@users.noreply.github.com>
2024-07-28 23:11:06 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/url-shortner/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
